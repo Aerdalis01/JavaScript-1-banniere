25c8f3aa06a6ff4818b938d506c50c27608f36a7
Aerdalis01 <caillonaudric@hotmail.fr>
2024-03-27 11:17:07 +0100

    Finalisation du mini projet

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log("connecté!")
+
+// Je selectionne et je stocke les éléments nécesséaire
+//bouton .btn-success;
+document.querySelector('.btn-success');
+
+
+const btnSuccess = document.querySelector('.btn-success');
+console.log(btnSuccess);
+
+const cookies = document.querySelector('.cookies');
+console.log(cookies);
+
+
+//je soumets la div à une action au clic
+
+btnSuccess.addEventListener('click',function(){
+  console.log("bonton cliqué");
+  cookies.style.opacity = "0";
+});
